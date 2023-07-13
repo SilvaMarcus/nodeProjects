6f66be24b1e972f43561fa7619f9c2e5ffffb3d2
Marcus-Silva <92951909+SilvaMarcus@users.noreply.github.com>
2023-07-12 22:17:00 -0300

    .

--- express/index.js
+++ express/index.js
@@ -1,18 +1,20 @@
 const express = require("express"); // Importando o Express 
-const app = express();
+const app = express(); // app recebendo o express
 
 
 app.get("/",function(req,res){
-    res.send("<h1> Bem vindo a pagina Home</h1>")
-});
+    res.send("<h1> Bem vindo a pagina Home</h1>") // resposta
+});  //metodo get para criar rota (express.get)
+
+
 
 app.get("/Compras",(req,res) => {
-    res.send("Bem vindo ao Compras")
-})
+    res.send("Bem vindo ao Compras") // resposta
+}) // criando rota com app.get (express.get)
 
 
-
-
+//usando o metodo listen do express para criar porta e abrir o servidor
+//abrindo servidor
 app.listen(5000, function(erro){
    
     if(erro){
