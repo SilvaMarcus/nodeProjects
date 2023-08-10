d51c565869941838ca25cfbddac81de381196f1a
Marcus-Silva <92951909+SilvaMarcus@users.noreply.github.com>
2023-08-10 19:24:34 -0300

    Update index.js
    avancando estudos

--- express/index.js
+++ express/index.js
@@ -2,21 +2,22 @@ const express = require("express"); // Importando o Express
 const app = express(); // app recebendo o express
 
 
-app.get("/",function(req,res){
-    res.send("<h1> Bem vindo a pagina Home</h1>") // resposta
+app.get("/:nome?",function(req,res){
+    let nome = req.params.nome
+    res.send(`Ola ${nome}`) // resposta
 });  //metodo get para criar rota (express.get)
 
 
 
 app.get("/Compras",(req,res) => {
-    res.send("Bem vindo ao Compras") // resposta
+    res.send("Bem vindo ao COMPRAS") // resposta
 }) // criando rota com app.get (express.get)
 
 
 //usando o metodo listen do express para criar porta e abrir o servidor
 //abrindo servidor
-app.listen(5000, function(erro){
-   
+app.listen(5000, function(erro){  
+
     if(erro){
     
         console.log("Acoteceu um erro")
